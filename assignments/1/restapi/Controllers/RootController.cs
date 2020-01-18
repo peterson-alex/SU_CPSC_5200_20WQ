e84e86ac02be69f3345929a572efce76fae2c65d
Alex Peterson <alexpeterson80@gmail.com>
2020-01-18 12:08:55 -0800

    Add support to root document for creating timesheets.

--- assignments/1/restapi/Controllers/RootController.cs
+++ assignments/1/restapi/Controllers/RootController.cs
@@ -1,11 +1,23 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using restapi.Models;
+using Microsoft.Extensions.Logging;
 
 namespace restapi.Controllers
 {
     public class RootController : Controller
     {
+        
+        private readonly TimesheetsRepository repository;
+
+        private readonly ILogger logger;
+
+        public RootController(ILogger<RootController> logger)
+        {
+            repository = new TimesheetsRepository();
+            this.logger = logger;
+        }
+        
         // GET api/values
         [Route("~/")]
         [HttpGet]
@@ -31,6 +43,25 @@ namespace restapi.Controllers
                     ApplicationRelationship.Version, "0.1"
                 }
             };
+        }
+
+        [Route("~/")]
+        [HttpPost]
+        [Produces(ContentTypes.Timesheet)]
+        [ProducesResponseType(typeof(Timecard), 200)]
+        public Timecard Create([FromBody] DocumentPerson person)
+        {
+            logger.LogInformation($"Creating timesheet for {person.ToString()}");
+
+            var timecard = new Timecard(person.Id);
+
+            var entered = new Entered() { Person = person.Id };
+
+            timecard.Transitions.Add(new Transition(entered));
+
+            repository.Add(timecard);
+
+            return timecard;
         }
     }
 }
